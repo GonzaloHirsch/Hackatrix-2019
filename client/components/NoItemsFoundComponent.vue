2e6bab7837ccedf4981c7257c304d66b1fff302f
nicbritos <nicbritos@hotmail.com>
2019-10-05 14:21:40 -0300

    Added 'NoItemsFound' component

---
+++ client/components/NoItemsFoundComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid>
+    <v-row align="center" justify="center">
+      <v-col md="6" lg="8">
+        <div class="pl-6 title text-center text--secondary">
+          No hay resultados. Prueba cambiando tu busqueda o utilizando otros filtros.
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "NoItemsFoundComponent"
+  };
+</script>
+
+<style scoped>
+
+</style>
