f8b997df3ecf8eb400ce73f0188fd475b184b8a4
Bruno De Angelis <brunordeangelis@gmail.com>
2019-10-05 15:32:33 -0300

    new navbar

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -14,7 +14,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins&display=swap' }
     ]
   },
   /*
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container mx-auto">
       <section class="tw-flex tw-flex-wrap tw--mx-4">
-          <article v-for="problem in problems" :key="problem.id" class="problem tw-w-1/3  tw-rounded-lg tw-cursor-pointer tw-my-6 tw-px-4" :class="{ 'tw-w-full': selectedProblem && selectedProblem.id == problem.id }" @click="selectedProblem = problem">
-              <div class="tw-flex tw-bg-gray-200 tw-rounded-lg">
-                  <img class="tw-object-cover tw-w-1/2" :src="problem.imageUrl" alt="">
-                  <div class="tw-w-1/2 tw-p-6">
-                    <h1 class="tw-break-words tw-font-bold tw-text-2xl tw-uppercase tw-tracking-wide tw-leading-tight">{{ problem.title }}</h1>
-                    <p>{{ problem.description }}</p>
+          <article v-for="problem in problems" :key="problem.id" class="problem tw-w-1/3  tw-rounded-lg tw-cursor-pointer tw-my-6 tw-px-4" :class="{ 'tw-w-full': isSelected(problem) }" @click="selectedProblem = problem">
+              <div class="tw-flex tw-bg-gray-200 tw-rounded-lg" :class="{ 'tw-flex-col': !isSelected(problem) }">
+                  <img class="tw-object-cover tw-w-1/2" :class="{ 'tw-w-full': !isSelected(problem) }" :src="problem.imageUrl" alt="">
+                  <div class="tw-w-1/2 tw-p-6" :class="{ 'tw-w-full': !isSelected(problem) }">
+                    <h1 class="tw-break-words tw-font-bold tw-text-2xl tw-uppercase tw-tracking-wide tw-leading-tight tw-mb-6">{{ problem.title }}</h1>
+                    <div :class="{ 'tw-hidden': !isSelected(problem) }">
+                        <p>{{ problem.description }}</p>
+                        <ul>
+                            <li v-for="(cause, i) in problem.causes" :key="i"><v-icon color="red">error</v-icon> {{ cause }}</li>
+                        </ul>
+                    </div>
                   </div>
               </div>
           </article>
@@ -21,6 +26,11 @@ export default {
     data() {
         return {
             selectedProblem: null
+        }
+    },
+    methods: {
+        isSelected(problem) {
+            return this.selectedProblem && this.selectedProblem.id == problem.id;
         }
     },
     computed: {
