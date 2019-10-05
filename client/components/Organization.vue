906ed1b7fd51f63ea9b075c74d2833ce25997ef6
nicbritos <nicbritos@hotmail.com>
2019-10-05 10:35:55 -0300

    Initial Organization View

---
+++ client/components/Organization.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header v-slot="{ open }">
+      <v-row>
+        <v-col cols="6">
+          <v-fade-transition leave-absolute>
+            <h3 v-if="!open" class="text-no-bold">
+              {{ item.name }}
+            </h3>
+
+            <h2 v-else class="text-no-bold">
+              Click para mas informacion
+            </h2>
+          </v-fade-transition>
+        </v-col>
+
+        <v-col cols="auto" class="text--secondary">
+          <v-fade-transition leave-absolute>
+            <span v-if="!open">
+              {{ item.description }}
+            </span>
+          </v-fade-transition>
+        </v-col>
+      </v-row>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <v-row>
+        <v-col cols="6">
+          <v-fade-transition leave-absolute>
+            <h3 v-if="!open" class="text-no-bold">
+              {{ item.name }}
+            </h3>
+
+            <h2 v-else class="text-no-bold">
+              {{ item.description }}
+            </h2>
+          </v-fade-transition>
+        </v-col>
+      </v-row>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+  export default {
+    name: "Organization",
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/organizaciones.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid>
+    <v-expansion-panels popout>
+      <Organization
+        v-for="item of items"
+        :key="item.name"
+        :item="item"
+      ></Organization>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script>
+  import Organization from "../components/Organization";
+
+  export default {
+    name: "organizaciones.vue",
+    components: { Organization },
+    data() {
+      return {
+        items: []
+      };
+    },
+    mounted() {
+      this.loadOrganizations();
+    },
+    methods: {
+      loadOrganizations() {
+        // setTimeout(() => {
+          this.items = [
+            {
+              title: "Greenpeace",
+              description: "Activistas para frenar el CC",
+              addresses: [],
+              phones: [],
+              mails: [],
+              img: ""
+            }
+          ]
+        // }, 1000);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
