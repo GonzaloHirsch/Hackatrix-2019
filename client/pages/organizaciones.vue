ae511811d9d57adfaba23b04eecae5325ee11620
nicbritos <nicbritos@hotmail.com>
2019-10-05 14:07:01 -0300

    Implemented search bar in Organizaciones

--- client/pages/organizaciones.vue
+++ client/pages/organizaciones.vue
@@ -7,12 +7,27 @@
         </div>
       </v-col>
     </v-row>
+
+    <v-row justify="center" dense class="pb-8 pl-6 pr-6 mb-n8">
+      <v-col md="6" lg="8">
+        <v-text-field
+          hide-details
+          prepend-icon="search"
+          single-line
+          class="ml-3"
+          placeholder="Start typing to Search"
+          v-model="search"
+          clearable
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
     <v-row justify="center">
       <v-col md="6" lg="8">
     <v-expansion-panels popout>
       <Organization
-        v-for="item of items"
-        :key="item.name"
+        v-for="item of filteredItems"
+        :key="item.title"
         :item="item"
       ></Organization>
     </v-expansion-panels>
@@ -29,11 +44,19 @@
     components: { Organization },
     data() {
       return {
-        items: []
+        items: [],
+        search: ""
       };
     },
     mounted() {
       this.loadOrganizations();
+    },
+    computed: {
+      filteredItems() {
+        if (this.search == null || this.search == "") return this.items;
+        let search = this.search.trim().toUpperCase();
+        return this.items.filter(v => v.title.toUpperCase().match(search));
+      }
     },
     methods: {
       loadOrganizations() {
