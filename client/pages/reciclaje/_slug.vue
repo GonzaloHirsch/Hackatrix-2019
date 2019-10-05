f04a5df07fdad50bd5d55a7d62b7912a4f330791
Bruno De Angelis <brunordeangelis@gmail.com>
2019-10-05 15:56:47 -0300

    added info to problems

--- client/pages/reciclaje/_slug.vue
+++ client/pages/reciclaje/_slug.vue
@@ -1,51 +1,10 @@
-<template><v-container fluid>
-  <v-row justify="center">
-    <v-col md="6" lg="8">
-      <div class="pl-6 display-1">
-        {{ currentCategory.name }}
-      </div>
-    </v-col>
-  </v-row>
-
-<!--  <v-row justify="center" dense class="pb-8 pl-6 pr-6 mb-n8">-->
-<!--    <v-col md="6" lg="8">-->
-<!--      <v-select-->
-<!--        chips-->
-<!--        label="Tipo de Tip"-->
-<!--        multiple-->
-<!--        solo-->
-<!--        :items="filterChips"-->
-<!--        clearable-->
-<!--        deletable-chips-->
-<!--        @change="chipFilterAdded"-->
-<!--        menu-props="offsetY"-->
-<!--      >-->
-<!--      </v-select>-->
-<!--    </v-col>-->
-<!--  </v-row>-->
-
-<!--  <div v-for="item in items" class="pb-8" :hidden="item.hidden">-->
-<!--    <v-row justify="center">-->
-<!--      <v-col md="6" lg="8">-->
-<!--        <div class="pl-6 headline">{{ item.type }}</div>-->
-<!--      </v-col>-->
-<!--    </v-row>-->
-
-<!--    <v-row justify="center">-->
-<!--      <v-col md="6" lg="8">-->
-<!--        <v-expansion-panels popout>-->
-<!--          <Tip-->
-<!--            v-for="tip in item.tips"-->
-<!--            :key="tip.title"-->
-<!--            :title="tip.title"-->
-<!--            :items="tip.items"-->
-<!--          ></Tip>-->
-<!--        </v-expansion-panels>-->
-<!--      </v-col>-->
-<!--    </v-row>-->
-<!--  </div>-->
-
-</v-container>
+<template>
+  <section class="w-full" style="height: 50vh;">
+    <img class="tw-object-cover tw-w-full tw-h-full" :src="currentCategory.img" alt="">
+    <h1 class="tw-mt-8 tw-text-center tw-text-5xl tw-font-bold tw-uppercase tw-tracking-wide ">
+      {{ currentCategory.name }}
+    </h1>
+  </section>
 </template>
 
 <script>
